d9545e64f1993125b01182757f8024634e7602c2
Adam Knuckey <adam@dolomite.io>
2021-11-03 12:53:45 -0400

    Added to sidenav, fixed trade link

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -7,7 +7,7 @@ import { useMedia } from 'react-use'
 import { transparentize } from 'polished'
 import { TYPE } from '../../Theme'
 import { withRouter } from 'react-router-dom'
-import { TrendingUp, List, PieChart, Disc } from 'react-feather'
+import { TrendingUp, List, PieChart, Disc, Code, Book } from 'react-feather'
 import Link from '../Link'
 import { useSessionStart } from '../../contexts/Application'
 import { useDarkModeManager } from '../../contexts/LocalStorage'
@@ -20,6 +20,7 @@ import TelegramLogo from '../../assets/telegram-logo.svg'
 import DiscordLogo from '../../assets/discord-logo.svg'
 import YouTubeLogo from '../../assets/youtube-logo.svg'
 import DiscourseLogo from '../../assets/discourse-logo.svg'
+import LinkedinLogo from '../../assets/linkedin-logo.svg'
 
 const Wrapper = styled.div`
   height: ${({ isMobile }) => (isMobile ? 'initial' : '100vh')};
@@ -194,6 +195,7 @@ function SideNav({ history }) {
             <HeaderText>
               <Link href="https://github.com/FATEx-DAO" target="_blank">
                 Code
+                {/*<Code size={14} />*/}
               </Link>
             </HeaderText>
             <HeaderText>
@@ -214,13 +216,14 @@ function SideNav({ history }) {
             <HeaderText>
               <Link href="https://fatexdao.gitbook.io/fatexdao/" target="_blank">
                 Green Paper
+                {/*<Book size={14} />*/}
               </Link>
             </HeaderText>
             <SocialLinks isDark={isDark}>
               <a href={'https://www.twitter.com/FATExDAO'} target={'_blank'} rel="noreferrer">
                 <img src={TwitterLogo} alt={'twitter logo'} />
               </a>
-              <a href={'https://www.reddit.com/r/FATExDAO'} target={'_blank'} rel="noreferrer">
+              <a href={'https://www.reddit.com/r/FATEx'} target={'_blank'} rel="noreferrer">
                 <img src={RedditLogo} alt={'reddit logo'} />
               </a>
               <a href={'https://fatexdao.medium.com'} target={'_blank'} rel="noreferrer">
@@ -237,6 +240,9 @@ function SideNav({ history }) {
               </a>
               <a href={'https://gov.daodiscourse.fatex.io/categories'} target={'_blank'} rel="noreferrer">
                 <img src={DiscourseLogo} alt={'discourse logo'} />
+              </a>
+              <a href={'https://www.linkedin.com/company/fatexdao'} target={'_blank'} rel="noreferrer">
+                <img src={LinkedinLogo} alt={'discourse logo'} />
               </a>
             </SocialLinks>
             <Toggle isActive={isDark} toggle={toggleDarkMode} />
